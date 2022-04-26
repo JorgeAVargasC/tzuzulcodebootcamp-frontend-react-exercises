812e1c44588cf4b9b26df3b893eeb73948cc22c8
JorgeVargas <javargas216@unicauca.edu.co>
2022-04-26 15:49:30 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Lista from "./components/Lista";
-import Tabla from "./components/Tabla";
+import Lista from "./components/Lista/Lista";
+import Tabla from "./components/Tabla/Tabla";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<Lista />
-			<Tabla />
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Lista />} />
+				<Route path="/lista" element={<Lista />} />
+				<Route path="/tabla" element={<Tabla />} />
+			</Routes>
+		</Router>
 	);
 }
 
