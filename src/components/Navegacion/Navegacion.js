a29e7a3103063d8e1b07443842ff034eb825905d
JorgeVargas <javargas216@unicauca.edu.co>
2022-04-26 19:08:42 -0500

    H2 text align center

--- src/components/Navegacion/Navegacion.js
+++ src/components/Navegacion/Navegacion.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 import './Navegacion.css'
 
 const Navegacion = () => {
-  return (
-    <div className='navbar'>
-      <h2>JV</h2>
-        <nav>
-            <Link to="/lista" className='links'>Lista</Link>
-            <Link to="/tabla" className='links'>Tabla</Link>
-        </nav>
-    </div>
-  )
+	return (
+		<div className='navbar'>
+			<h2>JV</h2>
+			<nav>				
+				<Link to="/lista" className='links'>Lista</Link>
+				<Link to="/tabla" className='links'>Tabla</Link>
+			</nav>
+		</div>
+	)
 }
 
 export default Navegacion
